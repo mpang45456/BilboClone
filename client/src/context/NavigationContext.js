b366d2eed99f1fcf800cf96b0feb78cb3861f1c1
Brian Yen <brianyen.na@gmail.com>
2020-06-22 13:38:45 +0800

    [EDIT] Delete NavigationContext.js

--- client/src/context/NavigationContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-// App-wide Navigation Context
-export const NavigationContext = createContext();
-
-// Hook to use the Context and gain access to variables set by Provider
-export function useNavigation() {
-    return useContext(NavigationContext);
-}
