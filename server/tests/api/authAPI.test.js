bf3d40e9814c39923bdb0e910bb4e5a587dc318a
Brian Yen <brianyen.na@gmail.com>
2020-06-25 17:29:52 +0800

    [EDIT] Refactored authAPI.test.js into 2 tests files (1 for /auth, 1 for /user). Refactored common code initially in authAPI.test.js into testUtils.js

--- server/tests/api/authAPI.test.js
+++ server/tests/api/authAPI.test.js
@@ -2,85 +2,56 @@ const request = require('supertest');
 const app = require('../../app');
 const { UserModel } = require('../../data/database');
 const { expect } = require('chai');
-const cookie = require('cookie');
-const { CookieAccessInfo } = require('cookiejar');
-
-const protectedEndpoint = '/test';
-const loginEndpoint = '/api/v1/auth/login';
-const logoutEndpoint = '/api/v1/auth/logout';
-const tokenEndpoint = '/api/v1/auth/token';
-const userEndpoint = '/api/v1/user';
 
 // Configure Test
 let testUsers = require('../../data/databaseBootstrap').users;
 const { PERMS } = require('../../routes/api/v1/auth/permissions');
 
-// const {
-
-// } = require('./testUtils');
-let serverPortNumber = process.env.PORT;
+const { DatabaseInteractor } = require('../../data/DatabaseInteractor');
+const { protectedEndpoint,
+        loginEndpoint, 
+        logoutEndpoint,
+        tokenEndpoint,
+        userEndpoint,
+        getAuthenticatedAgent, 
+        getCookieValueFromAgent,
+        parseCookiesFromResponse } = require('./testUtils');
 
 let authenticatedAdminAgent = null;
-const accessInfo = CookieAccessInfo();
 let accessToken = null;
 let refreshToken = null;
 let server = null;
-
-// Helper Functions
-function addUserToDatabase(user) {
-    const userObj = new UserModel({ username: user.username, 
-                                    permissions: user.permissions, 
-                                    name: user.name, 
-                                    position: user.position 
-    });
-    userObj.setPassword(user.password);
-    return userObj.save();
-}
-
-/**
- * Returns JSON object after parsing cookies in 
- * `res`'s header
- * 
- * The alternative method of retrieving the cookies
- * is to obtain them via the `getCookie` method
- * in `supertest.agent.jar`.
- * @param {Object} res 
- */
-function parseCookiesFromResponse(res) {
-    return cookie.parse(res.header['set-cookie'].join('; '));
-}
-
-const { DatabaseInteractor } = require('../../data/DatabaseInteractor');
 let dbi = null;
+
 beforeAll(async (done) => {
+    // Set up Database Connection before all tests
+    dbi = new DatabaseInteractor();
+    await dbi.initConnection();
+
     // Set up server before all tests
+    const serverPortNumber = process.env.PORT;
     server = app.listen(serverPortNumber, function() {
-        dbi = new DatabaseInteractor();
         done();
     })
 })
 
+/**
+ * Although the /auth endpoint does not modify 
+ * the User database, the tests should reset the 
+ * User database anyway because the protected endpoint
+ * being used to test successful authentication is 
+ * the /user endpoint.
+ */
 beforeEach(async (done) => {
     // Reset Database
     await dbi.clearModelData(UserModel);
     await dbi.addUsers(...testUsers);
-    // await UserModel.deleteMany({});
-    // testUsers.forEach(async (user) => {
-    //     try {
-    //         await addUserToDatabase(user);
-    //     } catch(error) {
-    //         console.error("Error occured: " + error);
-    //     }
-    // })
     
     // Login and Authenticate
     const admin = testUsers[0];
-    authenticatedAdminAgent = request.agent(server);
-    await authenticatedAdminAgent
-            .post(loginEndpoint)    
-            .send({ username: admin.username, password: admin.password });
-    accessToken = authenticatedAdminAgent.jar.getCookie('accessToken', accessInfo).value;
-    refreshToken = authenticatedAdminAgent.jar.getCookie('refreshToken', accessInfo).value; 
+    authenticatedAdminAgent = await getAuthenticatedAgent(server, admin.username, admin.password);
+    accessToken = getCookieValueFromAgent(authenticatedAdminAgent, 'accessToken');
+    refreshToken = getCookieValueFromAgent(authenticatedAdminAgent, 'refreshToken');
     done();
 })
 
@@ -258,6 +229,11 @@ describe('Testing /api/v1/auth/logout', () => {
     
     it("Should not be able to access protected endpoints after logout", 
         async (done) => {
+        // Able to access before logout
+        await authenticatedAdminAgent
+                .get(protectedEndpoint)
+                .expect(200)
+        
         await authenticatedAdminAgent
             .post(logoutEndpoint)
             .expect(200)
@@ -265,6 +241,7 @@ describe('Testing /api/v1/auth/logout', () => {
                 expect(res.text).to.be.equal("Logout Successful");
             })
 
+        // Not able to access after logout
         await authenticatedAdminAgent
                 .get(protectedEndpoint)
                 .expect(403)
@@ -289,283 +266,6 @@ describe('Testing /api/v1/auth/logout', () => {
                 .get(protectedEndpoint)
                 .expect(403)
         
-        return done();
-    })
-})
-
-describe('Testing /api/v1/auth/user endpoint', () => {
-    let newAdmin = {
-        "username": "newAdmin",
-        "password": "newAdminPassword",
-        "permissions": Object.keys(PERMS),
-        "name": "Gandalf",
-        "position": "Resident Wizard",
-        "reportsTo": "admin"
-    }
-
-    let newNonAdmin = {
-        "username": "newUser",
-        "password": "newUserPassword",
-        "permissions": [PERMS.SALES_READ, PERMS.PURCHASES_READ],
-        "name": "Thorin",
-        "position": "Resident Dwarf",
-        "reportsTo": "admin"
-    }
-
-    let authenticatedUnauthorizedAgent = null;
-
-    beforeEach(async (done) => {
-        // Create non-admin agent
-        const nonAdmin = testUsers[1];
-        authenticatedUnauthorizedAgent = request.agent(server);
-        await authenticatedUnauthorizedAgent
-                .post(loginEndpoint)    
-                .send({ username: nonAdmin.username, password: nonAdmin.password });
-        done();
-    })
-
-    // TODO: Add tests for authentication
-
-    it("User with USER_WRITE perm should be able to create new user", async (done) => {
-        // Create new user
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send(newAdmin)
-                .expect(200)
-        
-        // Should be able to log in as newly created user
-        await request(server)
-                .post(loginEndpoint)
-                .send({ username: newAdmin.username, password: newAdmin.password })
-                .expect(200)
-                .expect((res) => {
-                    let cookies = parseCookiesFromResponse(res);
-                    expect(cookies.accessToken).to.exist;
-                    expect(cookies.refreshToken).to.exist;
-                })
-        
-        return done();
-    })
-
-    it(`Newly created user should be in resource collection`, async (done) => {
-        let origLength = testUsers.length;
-
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send(newAdmin)
-                .expect(200);
-        
-        await authenticatedAdminAgent
-                .get(userEndpoint)
-                .expect((res) => {
-                    expect(res.body.length).to.equal(origLength + 1);
-                })
-
-        return done();
-    })
-
-    it(`User with USER_WRITE perm should not be able to 
-        create new user without required fields (excluding
-        permissions)`, async(done) => {
-        // Missing username field
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    password: newAdmin.password,
-                    permissions: newAdmin.permissions,
-                    name: newAdmin.name,
-                    position: newAdmin.position,
-                    reportsTo: newAdmin.reportsTo
-                })
-                .expect(400)
-        
-        // Missing password field
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    username: newAdmin.username,
-                    permissions: newAdmin.permissions,
-                    name: newAdmin.name,
-                    position: newAdmin.position,
-                    reportsTo: newAdmin.reportsTo
-                })
-                .expect(400)
-        
-        // Missing name field
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    username: newAdmin.username,
-                    password: newAdmin.password,
-                    permissions: newAdmin.permissions,
-                    position: newAdmin.position,
-                    reportsTo: newAdmin.reportsTo
-                })
-                .expect(400)
-        
-        // Missing username field
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    password: newAdmin.password,
-                    permissions: newAdmin.permissions,
-                    name: newAdmin.name,
-                    position: newAdmin.position,
-                    reportsTo: newAdmin.reportsTo
-                })
-                .expect(400)
-
-        // Missing reportsTo field
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    username: newAdmin.username,
-                    password: newAdmin.password,
-                    permissions: newAdmin.permissions,
-                    name: newAdmin.name,
-                    position: newAdmin.position
-                })
-                .expect(400)
-
-        done();
-    })
-
-    it(`User with USER_WRITE perm should be able 
-        to create new user with no permissions`, async (done) => {
-        // Although the permissions field is required, it
-        // defaults to [], which is considered valid
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send({
-                    username: newAdmin.username,
-                    password: newAdmin.password,
-                    name: newAdmin.name,
-                    position: newAdmin.position,
-                    reportsTo: newAdmin.reportsTo
-                })
-                .expect(200);
-
-        done();
-    })
-
-    it(`User with USER_WRITE perm should not be 
-        able to re-create existing user`, async (done) => {
-        // Re-create existing user
-        await authenticatedAdminAgent
-                .post(userEndpoint)
-                .send(testUsers[1])
-                .expect(400)
-                .expect((res) => {
-                    expect(res.text).to.be.equal('Unable to create new user');
-                })
-        
-        return done();
-    })
-
-    it(`User with USER_WRITE perm should be able 
-        to update password of existing user`, async (done) => {
-        let newPassword = "newPassword123";
-        await authenticatedAdminAgent
-                .patch(userEndpoint + `/${testUsers[1].username}`)
-                .send({ password: newPassword })
-                .expect(200)
-                .expect((res) => {
-                    expect(res.text).to.be.equal('Updated User Details');
-                })
-        
-        // User should be able to log in with new password
-        request(server)
-            .post(loginEndpoint)
-            .send({ username: testUsers[1].user, password: newPassword})
-            .expect(200)
-        
-        return done();
-    })
-
-    it("User with USER_WRITE perm should be able to delete existing user", async (done) => {
-        await authenticatedAdminAgent
-                .delete(userEndpoint + `/${testUsers[1].username}`)
-                .expect(200)
-                .expect((res) => {
-                    expect(res.text).to.be.equal('Deleted User');
-                })
-        
-        // User should no longer be able to log in
-        request(server)
-            .post(loginEndpoint)
-            .send({ username: testUsers[1].user, password: testUsers[1].password })
-            .expect(200)
-        
-        return done();
-    })
-
-    it(`Deleted user should not be in resource collection`, async (done) => {
-        let origLength = testUsers.length;
-
-        await authenticatedAdminAgent
-                .delete(userEndpoint + `/${testUsers[1].username}`)
-                .expect(200);
-        
-        await authenticatedAdminAgent
-                .get(userEndpoint)
-                .expect((res) => {
-                    expect(res.body.length).to.equal(origLength - 1);
-                })
-
-        return done();
-    })
-
-    it("User with USER_WRITE perm should be able to get list of all users", async (done) => {
-        await authenticatedAdminAgent
-                .get(userEndpoint)
-                .expect(200)
-                .expect((res) => {
-                    expect(res.body.length).to.be.equal(testUsers.length);
-                    expect(res.body[0].username).to.exist;
-                    expect(res.body[0].permissions).to.exist;
-                    expect(res.body[0].name).to.exist;
-                    expect(res.body[0].position).to.exist;
-                })
-        
-        return done();
-    })
-
-    it("User with USER_WRITE perm should be able to get single user's detail", async (done) => {
-        await authenticatedAdminAgent
-                .get(userEndpoint + `/${testUsers[1].username}`)
-                .expect(200)
-                .expect((res) => {
-                    expect(res.body.username).to.equal(testUsers[1].username);
-                    expect(res.body.role).to.equal(testUsers[1].role);
-                })
-        
-        return done();
-    })
-
-    it(`User without USER_WRITE perm should not 
-        be able access /api/v1/auth/user endpoints`, async (done) => {
-        await authenticatedUnauthorizedAgent
-                .post(userEndpoint)
-                .send(newAdmin)
-                .expect(403);
-        
-        await authenticatedUnauthorizedAgent
-                .patch(userEndpoint + `/${testUsers[0].username}`)
-                .send(newAdmin)
-                .expect(403);
-        
-        await authenticatedUnauthorizedAgent
-                .delete(userEndpoint + `/${testUsers[0].username}`)
-                .expect(403);
-        
-        await authenticatedUnauthorizedAgent
-                .get(userEndpoint)
-                .expect(403);
-
-        await authenticatedUnauthorizedAgent
-                .get(userEndpoint + `/${testUsers[0].username}`)
-                .expect(403);
-
         return done();
     })
 })
--- server/tests/api/testUtils.js
+++ server/tests/api/testUtils.js
@@ -1,18 +1,53 @@
 const request = require('supertest');
+const { CookieAccessInfo } = require('cookiejar');
+const cookie = require('cookie');
 
-export const protectedEndpoint = '/test';
+export const protectedEndpoint = '/api/v1/user';
 export const loginEndpoint = '/api/v1/auth/login';
 export const logoutEndpoint = '/api/v1/auth/logout';
 export const tokenEndpoint = '/api/v1/auth/token';
 export const userEndpoint = '/api/v1/user';
 
-const { CookieAccessInfo } = require('cookiejar');
-
-
-
-async function getAuthenticatedAgentAndTokens(server, username, password) {
+/**
+ * Returns a supertest agent that has already sent a POST
+ * request to the login endpoint with the provided credentials.
+ * The agent remains authenticated because it preserves the cookies
+ * (i.e. the access and refresh JWTs).
+ *  
+ * The correct credentials (username and password) must be 
+ * provided, otherwise the agent remains unauthenticated.
+ * 
+ * @param {Object} server (returned by app.listen())
+ * @param {String} username 
+ * @param {String} password 
+ */
+export async function getAuthenticatedAgent(server, username, password) {
     let agent = request.agent(server);
     await agent.post(loginEndpoint)
                .send({ username, password});
-    
+    return agent;
+}
+
+/**
+ * Returns the value of the cookie stored in the supertest
+ * agent's cookie jar.
+ * @param {supertest.agent} agent 
+ * @param {String} cookieName 
+ */
+export function getCookieValueFromAgent(agent, cookieName) {
+    const accessInfo = CookieAccessInfo();
+    return agent.jar.getCookie(cookieName, accessInfo).value;
+}
+
+/**
+ * Returns JSON object after parsing cookies in 
+ * `res`'s header
+ * 
+ * The alternative method of retrieving the cookies
+ * is to obtain them via the `getCookie` method
+ * in `supertest.agent.jar`.
+ * @param {Object} res 
+ */
+export function parseCookiesFromResponse(res) {
+    return cookie.parse(res.header['set-cookie'].join('; '));
 }
