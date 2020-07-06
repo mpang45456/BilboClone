5d803a4f2cac08d77403becac9dedd9d7011950a
Brian Yen <brianyen.na@gmail.com>
2020-07-07 01:22:02 +0800

    [EDIT] Refactor SupplierViewPageTabs into separate file. Clean up import statements

--- client/src/components/Supplier/SupplierViewPage/SupplierViewPage.js
+++ client/src/components/Supplier/SupplierViewPage/SupplierViewPage.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Spin, Descriptions, Tabs, Modal, Menu, message } from 'antd';
-const { TabPane } = Tabs;
+import { Spin, Descriptions, Modal, Menu, message } from 'antd';
 const { confirm } = Modal;
-import { DeleteOutlined, ExclamationCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { bax, useAuth, redirectToErrorPage, PERMS } from '../../../context/AuthContext';
 import CONFIG from '../../../config';
 import { BilboDescriptions, 
@@ -11,8 +10,7 @@ import { BilboDescriptions,
          BilboDivider, 
          EditableItem, 
          ShowMoreButton } from '../../UtilComponents';
-import PartsTabContent from './PartsTabContent';
-import PurchaseOrdersTabContent from './PurchaseOrdersTabContent';
+import SupplierViewPageTabs from './SupplierViewPageTabs';
 import PropTypes from 'prop-types';
 
 /**
@@ -180,87 +178,3 @@ DeleteSupplierShowMoreButton.propTypes = {
     disabled: PropTypes.bool.isRequired
 }
 
-/**
- * Component rendering the tabs section
- * of <SupplierViewPage />.
- * 
- * Note: Whenever the active tab changes, the 
- * `showMoreButton` associated with the tab 
- * changes it dropdown menu option. See class
- * `TabsShowMoreButton`.
- */
-function SupplierViewPageTabs(props) {
-    const [activeTab, setActiveTab] = useState('1');
-
-    return (
-        <Tabs defaultActiveKey={activeTab}
-              onChange={(activeKey) => setActiveTab(activeKey)}
-              tabBarExtraContent={<TabsShowMoreButton 
-                                    activeTab={activeTab}/>}>
-            <TabPane tab='All Parts' key='1'>
-                <PartsTabContent supplierid={props.supplierID}/>
-            </TabPane>
-            <TabPane tab='Purchase Orders' key='2'>
-                <PurchaseOrdersTabContent supplierid={props.supplierID}/>
-            </TabPane>
-        </Tabs>
-    )
-}
-SupplierViewPageTabs.propTypes = {
-    supplierID: PropTypes.string.isRequired,
-}
-
-/**
- * `showMoreButton` that changes its 
- * dropdown menu option according to the
- * value of `props.activeTab`. 
- * 
- * If `props.activeTab` === '1' ('All Parts' tab):
- * display 'Add a Part'
- * 
- * If `props.activeTab` === '2' ('Purchase Orders' tab):
- * display 'Add a Purchase Order'
- */
-function TabsShowMoreButton(props) {
-    const history = useHistory();
-    const { permissionsList } = useAuth();
-
-    const buttonClicked = ({item, key, keyPath, domEvent}) => {
-        if (key === 'addPart') {
-            history.push(`${CONFIG.PARTS_URL}add`);
-        } else if (key === 'addPurchaseOrder') {
-            // TODO: Add implementation for Purchase Orders
-            console.log('TO BE IMPLEMENTED');
-        }
-    }
-
-    const menu = (
-        <Menu onClick={buttonClicked}>
-            {
-                props.activeTab === '1'
-                ? <Menu.Item 
-                    key='addPart'
-                    icon={<PlusOutlined/>}>
-                    Add a Part
-                  </Menu.Item>
-                : <Menu.Item 
-                    key='addPurchaseOrder'
-                    icon={<PlusOutlined/>}>
-                    Add a Purchase Order
-                  </Menu.Item>
-            }
-        </Menu>
-    )
-    
-    // TODO: Update disabled value for Purchase Orders permissions
-    return (
-        <ShowMoreButton 
-            dropdownKey='tabShowMoreDropdown'
-            menu={menu}
-            disabled={props.activeTab === '1' && !permissionsList.includes(PERMS.PART_WRITE)}
-        />
-    )
-}
-TabsShowMoreButton.propTypes = {
-    activeTab: PropTypes.string.isRequired,
-}
