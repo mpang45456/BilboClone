f350adafc2fa3766de153563ba3812cdd5f47f5d
Brian Yen <brianyen.na@gmail.com>
2020-07-07 01:17:29 +0800

    [ADD] Add show more button to SupplierViewPage for adding a new part

--- client/src/components/Supplier/SupplierViewPage/SupplierViewPage.js
+++ client/src/components/Supplier/SupplierViewPage/SupplierViewPage.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { Spin, Descriptions, Tabs, Modal, Menu, message } from 'antd';
 const { TabPane } = Tabs;
 const { confirm } = Modal;
-import { DeleteOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+import { DeleteOutlined, ExclamationCircleOutlined, PlusOutlined } from "@ant-design/icons";
 import { bax, useAuth, redirectToErrorPage, PERMS } from '../../../context/AuthContext';
 import CONFIG from '../../../config';
 import { BilboDescriptions, 
@@ -125,14 +125,7 @@ export default function SupplierViewPage(props) {
 
             <BilboDivider />
 
-            <Tabs defaultActiveKey='1'>
-                <TabPane tab='All Parts' key='1'>
-                    <PartsTabContent supplierid={props.match.params.supplierID}/>
-                </TabPane>
-                <TabPane tab='Purchase Orders' key='2'>
-                    <PurchaseOrdersTabContent supplierid={props.match.params.supplierID}/>
-                </TabPane>
-            </Tabs>
+            <SupplierViewPageTabs supplierID={props.match.params.supplierID}/>
         </div>
     );
 }
@@ -172,11 +165,6 @@ function DeleteSupplierShowMoreButton(props) {
 
     const menu = (
         <Menu onClick={buttonClicked}>
-            <Menu.Item 
-                key='deleteSupplier'
-                icon={<DeleteOutlined/>}>
-                Delete Supplier
-            </Menu.Item>
         </Menu>
     )
     return (
@@ -192,3 +180,87 @@ DeleteSupplierShowMoreButton.propTypes = {
     disabled: PropTypes.bool.isRequired
 }
 
+/**
+ * Component rendering the tabs section
+ * of <SupplierViewPage />.
+ * 
+ * Note: Whenever the active tab changes, the 
+ * `showMoreButton` associated with the tab 
+ * changes it dropdown menu option. See class
+ * `TabsShowMoreButton`.
+ */
+function SupplierViewPageTabs(props) {
+    const [activeTab, setActiveTab] = useState('1');
+
+    return (
+        <Tabs defaultActiveKey={activeTab}
+              onChange={(activeKey) => setActiveTab(activeKey)}
+              tabBarExtraContent={<TabsShowMoreButton 
+                                    activeTab={activeTab}/>}>
+            <TabPane tab='All Parts' key='1'>
+                <PartsTabContent supplierid={props.supplierID}/>
+            </TabPane>
+            <TabPane tab='Purchase Orders' key='2'>
+                <PurchaseOrdersTabContent supplierid={props.supplierID}/>
+            </TabPane>
+        </Tabs>
+    )
+}
+SupplierViewPageTabs.propTypes = {
+    supplierID: PropTypes.string.isRequired,
+}
+
+/**
+ * `showMoreButton` that changes its 
+ * dropdown menu option according to the
+ * value of `props.activeTab`. 
+ * 
+ * If `props.activeTab` === '1' ('All Parts' tab):
+ * display 'Add a Part'
+ * 
+ * If `props.activeTab` === '2' ('Purchase Orders' tab):
+ * display 'Add a Purchase Order'
+ */
+function TabsShowMoreButton(props) {
+    const history = useHistory();
+    const { permissionsList } = useAuth();
+
+    const buttonClicked = ({item, key, keyPath, domEvent}) => {
+        if (key === 'addPart') {
+            history.push(`${CONFIG.PARTS_URL}add`);
+        } else if (key === 'addPurchaseOrder') {
+            // TODO: Add implementation for Purchase Orders
+            console.log('TO BE IMPLEMENTED');
+        }
+    }
+
+    const menu = (
+        <Menu onClick={buttonClicked}>
+            {
+                props.activeTab === '1'
+                ? <Menu.Item 
+                    key='addPart'
+                    icon={<PlusOutlined/>}>
+                    Add a Part
+                  </Menu.Item>
+                : <Menu.Item 
+                    key='addPurchaseOrder'
+                    icon={<PlusOutlined/>}>
+                    Add a Purchase Order
+                  </Menu.Item>
+            }
+        </Menu>
+    )
+    
+    // TODO: Update disabled value for Purchase Orders permissions
+    return (
+        <ShowMoreButton 
+            dropdownKey='tabShowMoreDropdown'
+            menu={menu}
+            disabled={props.activeTab === '1' && !permissionsList.includes(PERMS.PART_WRITE)}
+        />
+    )
+}
+TabsShowMoreButton.propTypes = {
+    activeTab: PropTypes.string.isRequired,
+}
